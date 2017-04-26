f604388b3d0a1ddf4088e3bbc6715a93544c040f
seafloormap <haven.carlson@gmail.com>
2017-04-26 14:45:09 -0400

    names php file

---
+++ names.php
@@ -0,0 +1,32 @@
+<?xml version = "1.0"?>
+<!DOCTYPE html PUBLIC "-//w3c//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<!-- dynamic.html 
+ -->
+<html xmlns = "http://www.w3.org/1999/xhtml">
+  <head>
+    <title>OT Names</title>
+  </head>
+  <body>
+
+	<h1> Names </h1>
+	<p>What are the first names of the members of your party?</p>
+  <form name="info" action="month.php" method="post">
+	1. <input type="text" name="wagonleader" value="">
+  <br>
+  	2. <input type="text" name="second" value="">
+  <br>
+  	3. <input type="text" name="third" value="">
+  <br>
+  	4. <input type="text" name="fourth" value="">
+  <br>
+  	5. <input type="text" name="fifth" value="">
+  <br>
+  <br>
+  <button type="submit">Month</button>
+  </form>
+    
+  
+  </body>
+</html>
