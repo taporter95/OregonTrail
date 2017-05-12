73c1592621d6acdb01fbbad2fad4a2770cde0d27
Timothy Porter <TimothyPorter@Timothys-MacBook-Pro-2.local>
2017-05-12 15:49:32 -0400

    test

--- game.php
+++ game.php
@@ -9,11 +9,11 @@
   <head>
     <title>OT Game</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script type="text/javascript" src="sessions.js"> </script>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="/resources/demos/style.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script type="text/javascript" src="sessions.js"> </script>
     <script type="text/javascript" src="trailFunctions.js"></script>
   </head>
 
