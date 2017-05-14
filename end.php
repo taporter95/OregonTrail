67377f06b1b9e1c9138e6162b0cc208c56b9bcb7
rthorpe1 <rthorpe1@umbc.edu>
2017-05-14 17:54:41 -0400

    Added End Picture and TrailInfo php files

---
+++ end.php
@@ -0,0 +1,26 @@
+<?xml version = "1.0"?>
+<!DOCTYPE html PUBLIC "-//w3c//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<!-- dynamic.html 
+ -->
+<html xmlns = "http://www.w3.org/1999/xhtml">
+  <head>
+    <title>OT End Game</title>
+	<link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+  <h1>Congratulations!</h1>
+  <img src="images/Willamette Valley.jpg" alt="Valley" style="width:728px;height:484px;">
+  <p>You have made it to Oregon.
+  <br>
+  Click below to see how many points you have recieved!
+  </p>
+  <form name="info" action="tally.html" method="post">
+  <button type="submit">Tally</button>
+  </form> 
+
+  
+  </body>
+</html>
---
+++ trailInfo.php
@@ -0,0 +1,66 @@
+<?xml version = "1.0"?>
+<!DOCTYPE html PUBLIC "-//w3c//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<!-- dynamic.html 
+ -->
+<html xmlns = "http://www.w3.org/1999/xhtml">
+  <head>
+    <title>The Oregon Trail</title>
+	<link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+  <h1> Trail Info </h1>
+  <p>Try taking a journey by covered wagon across 2000 miles
+  <br>
+  of plains, rivers, and mountains. Try! On the plains will
+  <br>
+  you slosh your oxen through mud and water-filled ruts or 
+  <br>
+  will you plod through dust six inches deep
+  </p>
+  <p>How will you cross the rivers? If you have money take
+  <br>
+  a ferry (if there is a ferry). Or, you can ford the river
+  <br>
+  and hope you and your wagon aren't swallowed alive!
+  </p>
+  <p> What about supplies? Well, if you're low on food
+  <br>
+  you can hunt. You might get a buffalo... you might. 
+  <br>
+  And there are bear in the mountains.
+  </p>
+  <p>At the Dalles, you can try navigating the Columbia River,
+  <br>
+  but if running the rapids with a makeshift raft makes you
+  <br>
+  queasy, better take the Barlow Road.
+  </p>
+  <p>If for some reason you don't survive -- your wagon burns,
+  <br>
+  or thieves steal your oxen, or you run out of provisions,
+  <br>
+  or you die of cholera -- don't give up! Try again...and
+  <br>
+  again... until your name is up with the others on The Oregon Top Ten.
+  </p>
+  <p>
+  The software team responsible for this adapation of Oregon trail includes:
+  <br>
+  <br>
+  Haven Carlson
+  <br>
+  Timothy Porter
+  <br>
+  Kurt Shuey
+  <br>
+  Roland Thorpe
+  <form name="info" action="title.php" method="post">
+  <button type="submit">Return to Title</button>
+  </form>
+    
+  
+  </body>
+</html>
