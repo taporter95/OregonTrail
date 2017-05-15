58ccfca01d368db9429b03b1d1bb0936dbbd0183
Timothy Porter <TimothyPorter@Timothys-MacBook-Pro-2.local>
2017-05-15 14:58:41 -0400

    fix

--- sessionGet.php
+++ sessionGet.php
@@ -7,6 +7,6 @@ $_SESSION["oxen"], $_SESSION["food"],$_SESSION["clothing"], $_SESSION["bait"],
 $_SESSION["wheels"],$_SESSION["axels"], $_SESSION["tongues"], $_SESSION["date"], $_SESSION["pace"], 
 $_SESSION["paceVal"], $_SESSION["rations"], $_SESSION["rationsVal"], 
 $_SESSION["health"], $_SESSION["weather"], $_SESSION["weatherCode"], 
-$_SESSION["locale"], $_SESSION["milesTraveled"], $_SESSION["milesToNext"], $_SESSION["profession"], json_encode($_SESSION["disease"]), $_SESSION["partySize"], $_SESSION["month"], $_SESSION["day"], $_SESSION["year"], $_SESSION["broken"] $_SESSION["split"]));
+$_SESSION["locale"], $_SESSION["milesTraveled"], $_SESSION["milesToNext"], $_SESSION["profession"], json_encode($_SESSION["disease"]), $_SESSION["partySize"], $_SESSION["month"], $_SESSION["day"], $_SESSION["year"], $_SESSION["broken"], $_SESSION["split"]));
 
 echo ($response);
