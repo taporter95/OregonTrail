1f5e06c3ca77f4f557097f1fce62b0045ee46b91
Timothy Porter <TimothyPorter@Timothys-MacBook-Pro-2.local>
2017-05-15 11:59:40 -0400

    test

--- riverOption.php
+++ riverOption.php
@@ -14,7 +14,38 @@
   <link rel="stylesheet" href="boxStyle.css">
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script type="text/javascript" src="trailFunctions.js"> </script>
+  
+			
+  </head>
+  <body>
+	<h1> River Option </h1>
+
+  <form name="crossForm" id="crossForm" action="crossing.php" method="post">
+  
+  <input type="hidden" name="depth" id="depth" >
+  
+	<input id="choose1" onclick="sendSession()" type="submit" name="choice" value="ford">
+    <input id="choose2" onclick="sendSession()" type="submit" name="choice" value="caulk">
+	<input id="choose3" onclick="sendSession()" type="submit" name="choice" value="ferry">
+ 
+  </form>
+  
+  <button id="wait"> Wait</button>
+  
+  <button id="info"> River Information</button>
+  
+  <div id="infoBox" >
+	<p id="riverInfo"> </p>
+	</div>
+  
+  <div id="waitBox" class="modalBox" title="Resting">
+  <p> How long would you like to wait? </p>
+  
+ <input type="number" id="waiting" min="0" value="0">
+  
+</div>
+  
+<script type="text/javascript" src="trailFunctions.js"> </script>
   
   
 <script>
@@ -26,42 +57,6 @@
  <script>
   $( function() {
 	 
-
-  	$("#wait").click(function(){
-		$("#waitBox").css("visibility","visible");
-		$("#waitBox").dialog({
-			modal: true,
-			buttons: [
-			{
-				text: "Wait",
-				click: function() {
-					// make time pass, heal people, and the such
-					for (var i = 0; i < $("#waiting").val(); i++){
-						
-						// ask how update Health works???
-						update_date();
-						food -= partySize * rationsVal;
-						updateHealth(true);
-						
-					}
-					//update weather
-					if ($("#waiting").val() > 0){
-						updateWeather();
-					var num = randomNumber(1, 100);
-					var change = parseFloat((Math.random() * (0.4 - 0.1) + 0.1).toFixed(1));
-					if (num <= 75){
-						riverDepth -= change;
-					}
-					else {
-						riverDepth += change;
-					}
-					}
-					$( this ).dialog( "close" );
-					}
-			}
-			]
-		});
-	});
   
 	$("#info").click(function(){
 		$("#infoBox").css("visibility","visible");
@@ -125,36 +120,5 @@
   
   } );
   </script>
-			
-  </head>
-  <body>
-	<h1> River Option </h1>
-
-  <form name="crossForm" id="crossForm" action="crossing.php" method="post">
-  
-  <input type="hidden" name="depth" id="depth" >
-  
-	<input id="choose1" onclick="sendSession()" type="submit" name="choice" value="ford">
-    <input id="choose2" onclick="sendSession()" type="submit" name="choice" value="caulk">
-	<input id="choose3" onclick="sendSession()" type="submit" name="choice" value="ferry">
- 
-  </form>
-  
-  <button id="wait"> Wait</button>
-  
-  <button id="info"> River Information</button>
-  
-  <div id="infoBox" >
-	<p id="riverInfo"> </p>
-	</div>
-  
-  <div id="waitBox" class="modalBox" title="Resting">
-  <p> How long would you like to wait? </p>
-  
- <input type="number" id="waiting" min="0" value="0">
-  
-</div>
-  
-  
   </body>
 </html>
