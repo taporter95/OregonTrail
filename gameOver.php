173fc44732aa3792aa11e59600b21ffbc6063499
Kurt Shuey <Kurt Shuey>
2017-05-16 11:28:21 -0400

    minigame shizz

---
+++ gameOver.php
@@ -0,0 +1,21 @@
+<?xml version = "1.0"?>
+<!DOCTYPE html PUBLIC "-//w3c//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<!-- dynamic.html 
+ -->
+<html xmlns = "http://www.w3.org/1999/xhtml">
+  <head>
+    <title>OT Game Over</title>
+	<link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+  <h1>You Died!</h1>
+  <form name="info" action="title.php" method="post">
+  <button type="submit">To Title Screen</button>
+  </form> 
+
+  
+  </body>
+</html>
