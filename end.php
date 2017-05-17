364d99134db919ab18f7ac544455a7011b5ab3ed
rthorpe1 <rthorpe1@umbc.edu>
2017-05-16 20:45:06 -0400

    added description

--- end.php
+++ end.php
@@ -1,3 +1,11 @@
+<?php
+/*
+End File for the Oregon Trail Game
+This file is reached when a player
+clears the game. It congratulates
+the player and leads to the tally page.
+*/
+?>
 <?xml version = "1.0"?>
 <!DOCTYPE html PUBLIC "-//w3c//DTD XHTML 1.1//EN"
   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
